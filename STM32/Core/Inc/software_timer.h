9e5f56ee525f818fb7aa3c3e91a4baa2bfcdcd3f
HuyNguyen <huy.nguyenquang116@gmail.com>
2023-11-30 15:04:22 +0700

    add structure of scheduler for lab 4

---
+++ STM32/Core/Inc/software_timer.h
@@ -0,0 +1,37 @@
+/*
+ * softwareTimer.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: admin
+ */
+
+#ifndef INC_SOFTWARETIMER_H_
+#define INC_SOFTWARETIMER_H_
+
+////////////////////////////////////////////////////
+extern int timer1_flag;                   //
+                                          // FOR LANE 1
+void setTimer1(int duration);             //
+void timer1Run();                         //
+////////////////////////////////////////////////////
+extern int timer2_flag;                   //
+										  // FOR LANE 2
+void setTimer2(int duration);             //
+void timer2Run();                         //
+///////////////////////////////////////////////////
+extern int timer3_flag;                   //
+										  // FOR COUNT DOWN 7 SEG LANE 1
+void setTimer3(int duration);			  //
+void timer3Run();						  //
+///////////////////////////////////////////////////
+extern int timer4_flag;                   //
+										  // FOR SCAN 4 7-SEG
+void setTimer4(int duration);			  //
+void timer4Run();						  //
+///////////////////////////////////////////////////
+extern int timer5_flag;                   //
+										  // FOR COUNT DOWN 7 SEG LANE 2
+void setTimer5(int duration);			  //
+void timer5Run();						  //
+///////////////////////////////////////////////////
+#endif /* INC_SOFTWARETIMER_H_ */
